bd3cbded4cf3dbd4246c00f232d9b68f4efad7ca
Akuli <akuviljanen17@gmail.com>
2016-10-10 15:08:18 +0300

    gtk css coloring

--- bananagui/bases/gtk3/bases.py
+++ bananagui/bases/gtk3/bases.py
@@ -3,11 +3,38 @@ from gi.repository import Gtk
 
 class Widget:
 
+    def __init__(self, **kwargs):
+        widget = self['real_widget']
+
+        # I have no idea why GTK+ makes changing the background color in
+        # a non-deprecated (not-removed) way such a pain.
+        self.__css = {}
+        self.__provider = Gtk.CssProvider()
+        context = self['real_widget'].get_style_context()
+        context.add_provider(self.__provider,
+                             Gtk.STYLE_PROVIDER_PRIORITY_USER)
+
+        super().__init__(**kwargs)
+
+    def __update_css(self):
+        words = []
+        for item in self.__css.items():
+            words.append('%s: %s;' % item)
+        css = '* { %s }' % ' '.join(words)
+        self.__provider.load_from_data(css.encode('utf-8'))
+
     def _bananagui_set_tooltip(self, tooltip):
         self['real_widget'].set_tooltip_text(tooltip)
 
-    def _bananagui_set_background(self, background):
-        
+    def _bananagui_set_background(self, color):
+        if color is None:
+            try:
+                del self.__css['background-color']
+            except KeyError:
+                pass
+        else:
+            self.__css['background-color'] = color.rgbstring
+        self.__update_css()
 
 
 class Parent:
