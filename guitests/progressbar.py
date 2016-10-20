910bdf5522d406292ad5d71f27ee082ecdf67ea3
Akuli <akuviljanen17@gmail.com>
2016-10-20 22:01:40 +0300

    progressbar test

---
+++ guitests/progressbar.py
@@ -0,0 +1,31 @@
+import functools
+
+import bananagui
+from bananagui import gui
+
+
+def set_progress(progressbar, event):
+    progressbar['progress'] = event.widget['value'] / 100
+
+
+def main():
+    with gui.Window(title="Scrollbar test") as window:
+        box = gui.Box.vertical(window)
+        window['child'] = box
+
+        progressbar = gui.Progressbar(
+            box, orientation=bananagui.HORIZONTAL, expand=(True, False))
+        box['children'].append(progressbar)
+
+        box['children'].append(gui.Dummy(box))
+
+        spinbox = gui.Spinbox(box, expand=(True, False))
+        spinbox['value.changed'].append(functools.partial(set_progress, progressbar))
+        box['children'].append(spinbox)
+
+        window['on_destroy'].append(gui.quit)
+        gui.main()
+
+
+if __name__ == '__main__':
+    main()
