863c8546ff30de027a857e6bce85b4aa93226371
Akuli <akuviljanen17@gmail.com>
2016-10-25 18:50:58 +0300

    gtk progressbar

--- bananagui/bases/gtk3/progress.py
+++ bananagui/bases/gtk3/progress.py
@@ -19,34 +19,51 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from gi.repository import Gtk
+from gi.repository import Gtk, GLib
 
 
 class Progressbar:
 
     def __init__(self, parent, **kwargs):
-        raise NotImplementedError  # TODO
+        widget = Gtk.ProgressBar()
+        self.real_widget.raw_set(widget)
+        super().__init__(parent, **kwargs)
 
     def _bananagui_set_progress(self, progress):
-        ...
+        self['real_widget'].set_fraction(progress)
 
 
 class BouncingProgressbar:
-
-    def __init__(self, parent, **kwargs):
-        raise NotImplementedError  # TODO
-
-    def _bananagui_set_bouncing(self, bouncing):
-        ...
-
-
-class Spinner:
 
     def __init__(self, parent, **kwargs):
         # We need to hide the spinner when it's not spinning. Rest of
         # BananaGUI calls real_widget.show() when the widget is added
         # somewhere, so we need to create a container for it and add the
         # real spinner inside it.
+        self.__bar = Gtk.ProgressBar()
+        container = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        container.pack_start(self.__bar, True, True, 0)
+        self.real_widget.raw_set(container)
+        super().__init__(parent, **kwargs)
+
+    def __on_timeout(self):
+        if not self['bouncing']:
+            return False
+        self.__bar.pulse()
+        return True  # Run this again.
+
+    def _bananagui_set_bouncing(self, bouncing):
+        if bouncing:
+            self.__bar.show()
+            GLib.timeout_add(100, self.__on_timeout)
+        else:
+            self.__bar.hide()
+
+
+class Spinner:
+
+    def __init__(self, parent, **kwargs):
+        # See the BouncingProgressbar comment.
         self.__spinner = Gtk.Spinner()
         container = Gtk.Box()
         container.pack_start(self.__spinner, True, True, 0)
