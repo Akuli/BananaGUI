8099723159fd85d2da1293613e95235cf7e81ffd
Akuli <akuviljanen17@gmail.com>
2016-12-04 21:41:40 +0200

    testing stuff in wine

--- bananagui/bases/tkinter/font.py
+++ bananagui/bases/tkinter/font.py
@@ -23,4 +23,7 @@ from tkinter import font
 
 
 def get_families():
-    return font.families()
+    # For some reason, some families start with a '@' on Windows. I'm
+    # not sure why, but let's ignore them for now.
+    return (family for family in font.families()
+            if not family.startswith('@'))
