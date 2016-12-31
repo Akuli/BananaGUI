f646b95a9e0f12227e855edc453f14213ed26f7c
Akuli <akuviljanen17@gmail.com>
2017-01-01 00:12:41 +0200

    test a bananagui.load() error

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,4 +5,8 @@ import bananagui
 
 @pytest.fixture(scope='session')
 def dummywrapper():
-    bananagui.load('tests.dummywrapper')
+    # The load() method can't be called twice, so setting up the dummy
+    # wrapper and testing it need to be combined.
+    bananagui.load('invalid-module', 'this-is-lol', 'tests.dummywrapper')
+    with pytest.raises(RuntimeError):
+        bananagui.load('tests.dummywrapper')    # cannot call it twice
