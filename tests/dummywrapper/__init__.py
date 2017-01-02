cab217e8fec7981bdb1ae9081acadb9df8fd8628
Akuli <akuviljanen17@gmail.com>
2017-01-02 15:12:21 +0200

    explaing why

--- tests/dummywrapper/__init__.py
+++ tests/dummywrapper/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Akuli
+# Copyright (c) 2016-2017 Akuli
 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -19,5 +19,4 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-"""A BananaGUI wrapper that does nothing."""
-# TODO: How is this useful?
+"""A BananaGUI wrapper that does nothing for testing."""
