836551f6a63739a23efb640cd03c99d1ed06a5ce
Akuli <akuviljanen17@gmail.com>
2017-01-02 15:10:20 +0200

    removing duplicate tests, adding a copyright notice

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2016-2017 Akuli
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import pytest
 
 from bananagui import types
@@ -151,12 +172,6 @@ def test_add_property_errors():
     d.intpair = [2, 4]
     with pytest.raises(TypeError):
         d.intpair = {2, 4}      # must be a sequence
-    with pytest.raises(TypeError):
-        d.intpair = iter([2, 4])
-    with pytest.raises(ValueError):
-        d.string = None
-    with pytest.raises(TypeError):
-        d.string = 123
     with pytest.raises(ValueError):
         d.string = None
     with pytest.raises(TypeError):
