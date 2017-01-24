835eda0f9473c3faccdaed7078d34f3afc6dade7
Akuli <akuviljanen17@gmail.com>
2017-01-24 11:40:36 +0200

    oops

--- bananagui/wrappers/defaults/msgbox.py
+++ bananagui/wrappers/defaults/msgbox.py
@@ -25,13 +25,14 @@ from bananagui import widgets
 
 def message(icon, parentwindow, message, title, buttons, defaultbutton):
     # TODO: do something with the icon?
-    def done(text):
+    def on_click(text):
         nonlocal result
         result = text
         dialog.close()
 
-    dialog = widgets.Dialog(parentwindow, title=title, minimum_size=(350, 150))
+    result = None
 
+    dialog = widgets.Dialog(parentwindow, title=title, minimum_size=(350, 150))
     mainbox = widgets.Box()
     dialog.add(mainbox)
 
@@ -42,16 +43,16 @@ def message(icon, parentwindow, message, title, buttons, defaultbutton):
     focus_this = None
     for buttontext in buttons:
         button = widgets.Button(text=buttontext)
-        button.on_click.connect(done, buttontext)
+        button.on_click.connect(on_click, buttontext)
         buttonbox.extend([widgets.Dummy(), button, widgets.Dummy()])
         if buttontext == defaultbutton:
             focus_this = button
     if focus_this is not None:
         focus_this.focus()
 
-    dialog.on_close.connect(done, None)
+    dialog.on_close.connect(dialog.close)
     dialog.wait()
-    return result   # noqa
+    return result
 
 
 # TODO: font dialog.
